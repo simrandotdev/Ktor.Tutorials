8b1f727639d8bde5535cec19f965d691b4072974
Simran <simran5590@gmail.com>
2021-05-23 23:22:06 -0700

    KtorRequests: Learning about call.request

---
+++ KtorRequests/src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,23 @@
+package com.example.plugins
+
+import io.ktor.routing.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun Application.configureRouting() {
+
+    routing {
+        get("/") {
+            println("URI: ${call.request.uri}") // '/'
+            println("Headers: ${call.request.headers.names()}")
+            println("User-Agent: ${call.request.headers["User-Agent"]}")
+            println("Accept: ${call.request.headers["Accept"]}")
+            println("Query Params: ${call.request.queryParameters.names()}")
+            println("Name: ${call.request.queryParameters["name"]}")
+            println("Email: ${call.request.queryParameters["email"]}")
+
+            call.respondText("Hello World!")
+        }
+    }
+}
