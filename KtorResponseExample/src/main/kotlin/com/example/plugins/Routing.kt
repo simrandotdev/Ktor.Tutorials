9f823f14ba22759aaeb022afc3203497882a80a9
Simran <simran5590@gmail.com>
2021-05-26 21:35:00 -0700

    Added Example for headers and file download

--- KtorResponseExample/src/main/kotlin/com/example/plugins/Routing.kt
+++ KtorResponseExample/src/main/kotlin/com/example/plugins/Routing.kt
@@ -6,6 +6,8 @@ import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
 import kotlinx.serialization.Serializable
+import java.io.File
+import java.util.*
 
 fun Application.configureRouting() {
 
@@ -13,6 +15,27 @@ fun Application.configureRouting() {
         get("/") {
             val responseObject = UserResponse("John", "john@gmail.com")
             call.respond(responseObject)
+        }
+
+
+        get("/headers") {
+            call.response.headers.append("UUID", UUID.randomUUID().toString())
+            call.response.headers.append("Request-Number", Random().nextInt(1000000).toString())
+            call.respondText("Request successful")
+        }
+
+        get("/files") {
+            val file = File("files/americano.jpg")
+            // Content-Disposition header tells whether the content
+            // will be "inline" (Display in the browser itself.)
+            // will be "attachment" (Gets downloaded and saved to your local machine)
+            call.response.header(
+                HttpHeaders.ContentDisposition,
+                ContentDisposition.Attachment.withParameter(
+                    ContentDisposition.Parameters.FileName, "americano.png"
+                ).toString()
+            )
+            call.respondFile(file)
         }
     }
 }
