a8b77adcc5f485f27a59ef7337a1bc230c8f2c2c
Simran <simran5590@gmail.com>
2021-05-29 12:54:24 -0700

    Headers and File download

--- KtorResponseExample/src/main/kotlin/com/example/plugins/Routing.kt
+++ KtorResponseExample/src/main/kotlin/com/example/plugins/Routing.kt
@@ -17,24 +17,35 @@ fun Application.configureRouting() {
             call.respond(responseObject)
         }
 
-
         get("/headers") {
-            call.response.headers.append("UUID", UUID.randomUUID().toString())
-            call.response.headers.append("Request-Number", Random().nextInt(1000000).toString())
-            call.respondText("Request successful")
+            call.response.headers.append("server-name", "KtorServer")
+            call.response.headers.append("chocolate", "I love it.")
+            call.respondText("Headers Attached")
         }
 
-        get("/files") {
+        get("/fileDownload") {
             val file = File("files/americano.jpg")
-            // Content-Disposition header tells whether the content
-            // will be "inline" (Display in the browser itself.)
-            // will be "attachment" (Gets downloaded and saved to your local machine)
+
             call.response.header(
                 HttpHeaders.ContentDisposition,
                 ContentDisposition.Attachment.withParameter(
-                    ContentDisposition.Parameters.FileName, "americano.png"
+                    ContentDisposition.Parameters.FileName, "downloadableImage.jpg"
                 ).toString()
             )
+
+            call.respondFile(file)
+        }
+
+        get("/fileOpen") {
+            val file = File("files/expresso.jpg")
+
+            call.response.header(
+                HttpHeaders.ContentDisposition,
+                ContentDisposition.Inline.withParameter(
+                    ContentDisposition.Parameters.FileName, "openImage.jpg"
+                ).toString()
+            )
+
             call.respondFile(file)
         }
     }
